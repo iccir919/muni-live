e8d85c33db2c2b7cdab629cf7c84bbd50ea470a4
Neil Ricci <iccir919@gmail.com>
2018-01-09 13:16:48 -0800

    Insert proxy server URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,9 +3,13 @@ import Rx from "rxjs";
 
 const API_URL = "http://webservices.nextbus.com/service/publicJSONFeed?command=vehicleLocations&a=sf-muni";
 
+const proxy_URL = 'https://neilmunimap.herokuapp.com/proxy?url=';
+
+const APIurlWithProxy = proxy_URL + API_URL;
+
 export const getVehicles = () =>
   new Promise((resolve, reject) => {
-    axios.get(API_URL).then(response => resolve(response.data));
+    axios.get(APIurlWithProxy).then(response => resolve(response.data));
   });
 
 export const timer = Rx.Observable.timer(0, 2000);
