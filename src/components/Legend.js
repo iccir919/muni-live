7bcda8c81eb011cec99451e184b3b6a46b0b7d36
Neil Ricci <iccir919@gmail.com>
2018-01-09 18:28:24 -0800

    Improve Legend description

--- src/components/Legend.js
+++ src/components/Legend.js
@@ -6,15 +6,15 @@ import { getRgbForValue } from "../utils/color.js";
 
 const Legend = () => (
   <div className="legend">
-    <h1 className="legend__title">Legend</h1>
+    <h1 className="legend__title">Vehicle Location Legend</h1>
     <div className="legend-line">
       <span>
-        <MarkerSvg color={getRgbForValue(50)} />Data 50s ago
+        <MarkerSvg color={getRgbForValue(50)} />Data from >= 50s ago
       </span>
     </div>
     <div className="legend-line">
       <span>
-        <MarkerSvg color={getRgbForValue(0)} />Data 0s ago
+        <MarkerSvg color={getRgbForValue(0)} />Data from 0s ago
       </span>
     </div>
   </div>
